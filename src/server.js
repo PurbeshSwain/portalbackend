ba93f549770e12dbfc61961917353ad60874a69b
Purbesh <purbeshswain5@gmail.com>
2023-06-18 01:43:31 +0530

    cors added

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const connectDB = require('./db');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 // Middleware
 app.use(express.json());
